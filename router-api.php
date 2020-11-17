0e17c8fab5ba7bf4ddddf4d70e7a648ed58e2bb8
carohansen73 <laaflaka.8@hotmail.com>
2020-11-17 15:31:31 -0300

    router -Api

--- router-api.php
+++ router-api.php
@@ -1,7 +1,15 @@
 <?php
 //incluyo el router de la libreria
 include_once 'libs/Router.php';
+include_once 'app/api/ApiController.php';
 
-//creo el nuuevo router
+//creo el nuevo router
 $router = new Router();
 
+//defino la tabla de ruteo (case)
+$router->addRoute('comentarios', 'GET', 'ApiController', 'getAll');
+$router->addRoute('comentarios/:ID', 'GET', 'ApiController', 'get');
+$router->addRoute('comentarios/:ID', 'DELETE', 'ApiController', 'delete');
+
+//rutear
+$router->route($_REQUEST['resource'], $_SERVER['REQUEST_METHOD']);
